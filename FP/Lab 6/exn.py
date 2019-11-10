b5ebd7295ef30b81b3c49098d7d4fa10f5efcca7
Alex Ionescu <alxi.2001@gmail.com>
2019-11-10 18:11:30 +0200

    Add custom exceptions & start implementing Services

---
+++ FP/Lab 6/exn.py
@@ -0,0 +1,8 @@
+class DuplicateIdError(Exception):
+  pass
+
+class InexistentItemError(Exception):
+  pass
+
+class MovieNotAvailableError(Exception):
+  pass
--- FP/Lab 6/services.py
+++ FP/Lab 6/services.py
@@ -1,6 +1,63 @@
 from domain import populate_rand
+from exn import *
+from action import *
 
 class Services:
   def __init__(self, populate = False):
     self.__movies, self.__clients, self.__rentals = populate_rand() if populate else {}, {}, {}
     self.__done_actions, self.__undone_actions = [], []
+
+  def __do(self, action):
+    action.apply()
+    self.__done_actions.append(action)
+    self.__undone_actions.clear()
+
+  def add_client(self, client):
+    d = self.__clients
+
+    if client.id() in d:
+      raise DuplicateIdError()
+    else:
+      self.__do(AddAction(d, client))
+
+  def add_movie(self, movie):
+    d = self.__movies
+
+    if movie.id() in d:
+      raise DuplicateIdError()
+    else:
+      self.__do(AddAction(d, movie))
+
+  def update_client(self, client):
+    d = self.__clients
+
+    if client.id() not in d:
+      raise InexistentItemError()
+    else:
+      old = d[client.id()]
+      self.__do(UpdateAction(d, old, client))
+      
+  def update_movie(self, movie):
+    d = self.__movies
+
+    if movie.id() not in d:
+      raise InexistentItemError()
+    else:
+      old = d[movie.id()]
+      self.__do(UpdateAction(d, old, movie))
+
+  def remove_client(self, client):
+    d = self.__clients
+
+    if client.id() not in d:
+      raise InexistentItemError()
+    else:
+      self.__do(RemoveAction(d, client))
+      
+  def remove_movie(self, movie):
+    d = self.__movies
+
+    if movie.id() not in d:
+      raise InexistentItemError()
+    else:
+      self.__do(RemoveAction(d, movie))
