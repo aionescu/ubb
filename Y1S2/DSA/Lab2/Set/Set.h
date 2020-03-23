0e0b19fda92134129e82cb89e2f9e3199e8c6720
Alex Ionescu <alxi.2001@gmail.com>
2020-03-24 00:57:02 +0200

    Add time complexities

--- Y1S2/DSA/Lab2/Set/Set.h
+++ Y1S2/DSA/Lab2/Set/Set.h
@@ -19,20 +19,25 @@ public:
   //implicit constructor
   Set() = default;
 
+  // O(n)
   //adds an element to the set
   //returns true if the element was added, false otherwise (if the element was already in the set and it was not added)
   bool add(TElem e);
 
+  // O(n)
   //removes an element from the set
   //returns true if e was removed, false otherwise
   bool remove(TElem e);
 
+  // O(n)
   //checks whether an element belongs to the set or not
   bool search(TElem elem) const;
 
+  // O(1)
   //returns the number of elements;
   int size() const;
 
+  // O(1)
   //check whether the set is empty or not;
   bool isEmpty() const;
 
