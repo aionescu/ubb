34ce4befcffd333c950e92cecd2fdc05dde65029
Alex Ionescu <alxi.2001@gmail.com>
2020-04-09 22:05:11 +0300

    Delete coverage.sh

--- Y1S2/OOP/Lab7/newfound-purposes/coverage.sh
+++
@@ -1,9 +0,0 @@
-g++ -std=c++17 -Wall -Wextra -pedantic --coverage $1
-
-gcov $1
-./a.out
-
-lcov -c --directory . --output-file Coverage.info
-genhtml Coverage.info --output-directory coverage-html
-
-rm -r *.gcov *.gcno *.gcda *.info a.out coverage-html
