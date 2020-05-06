ecb3b98c7775157297b4cbc1297a62d1efa69461
Alex Ionescu <alxi.2001@gmail.com>
2020-05-06 11:35:24 +0300

    Add README & LICENSE; Rename folders

---
+++ README.md
@@ -0,0 +1,8 @@
+# Uni
+
+My university assignments and projects
+
+## License
+
+This repository is licensed under the terms of the MIT License.
+For more details, see [the license file](LICENSE.txt).
--- Y1S2/OS/Test2/odd-even.sh
+++ Y1S2/OS/Test2/odd-even.sh
@@ -4,8 +4,8 @@
 for ((i = 3; i <= $#; ++i)); do
   for file in $(ls ${!i}); do
     if [ -f $file ]; then
-      cat $file | grep -o "[13579]" >> $1
-      cat $file | grep -o "[02468]" >> $2
+      cat $file | grep -o "[13579]" >> "$1"
+      cat $file | grep -o "[02468]" >> "$2"
     fi
   done
 done
