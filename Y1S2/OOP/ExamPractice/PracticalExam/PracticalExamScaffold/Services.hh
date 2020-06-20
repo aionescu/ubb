6f8b34a30afa0e46627dccfe86679c167c50b3f9
Alex Ionescu <alxi.2001@gmail.com>
2020-06-20 03:45:49 +0300

    Add PracticalExamScaffold in OOP folder

---
+++ Y1S2/OOP/ExamPractice/PracticalExam/PracticalExamScaffold/Services.hh
@@ -0,0 +1,37 @@
+#ifndef SERVICES_HH
+#define SERVICES_HH
+
+#include "Repo.hh"
+
+template <typename TRepo>
+class Services {
+  TRepo _repo;
+
+public:
+  template <typename TEntity>
+  void loadFromFile(std::string path) {
+    _repo.Repo<TEntity>::loadFromFile(path);
+  }
+
+  template <typename TEntity>
+  void saveToFile(std::string path) {
+    _repo.Repo<TEntity>::saveToFile(path);
+  }
+
+  template <typename TEntity>
+  void add(TEntity entity) {
+    _repo.Repo<TEntity>::add(entity);
+  }
+
+  template <typename TEntity>
+  void update(TEntity oldEntity, TEntity newEntity) {
+    _repo.Repo<TEntity>::update(oldEntity, newEntity);
+  }
+
+  template <typename TEntity>
+  void remove(TEntity entity) {
+    _repo.Repo<TEntity>::remove(entity);
+  }
+};
+
+#endif
