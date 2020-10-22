7a9ca8559e32162961d1c2ed9b9d72096adc36d2
Alex Ionescu <alxi.2001@gmail.com>
2020-10-23 02:59:29 +0300

    Add FLP Lab2 HW

---
+++ Y2S1/FLP/Lab2/p7.pl
@@ -0,0 +1,41 @@
+% a. Write a predicate to compute the intersection of two sets.
+% b. Write a predicate to create a list (m, ..., n) of all integer numbers from the interval [m, n].
+
+% a)
+% elem(a, l1..ln) =
+%   { False, if n = 0
+%   , True, if a = l1
+%   , elem(a, l2..ln), otherwise
+%   }
+%
+% intersect(l1..ln, k1..km) =
+%   { [], if n = 0
+%   , l1 : intersect(l2..ln, k1..km), if elem(l1, k1..km)
+%   , intersect(l2..ln, k1..km), otherwise
+%   }
+
+elem(A, [A|_]).
+elem(A, [_|T]) :- elem(A, T).
+
+intersect([], _, []).
+intersect([H|T], L, [H|R]) :- elem(H, L), intersect(T, L, R).
+intersect([_|T], L, R) :- intersect(T, L, R).
+
+:- E = 1, L = [1, 2, 3], elem(E, L), format('~w is elem of ~w~n', [E, L]).
+:- X = [1, 2], Y = [1, 2, 3, 4], intersect(X, Y, Z), format('intersect(~w, ~w) is ~w~n', [X, Y, Z]).
+:- X = [], Y = [1, 2], intersect(X, Y, Z), format('intersect(~w, ~w) is ~w~n', [X, Y, Z]).
+:- X = [1, 2], Y = [], intersect(X, Y, Z), format('intersect(~w, ~w) is ~w~n', [X, Y, Z]).
+
+% b)
+% iota(m, n) =
+%   { [], if m > n
+%   , m : iota(m + 1, n), otherwise
+%   }
+
+iota(M, N, []) :- M @> N.
+iota(M, N, [M|T]) :- Mn is M + 1, iota(Mn, N, T).
+
+:- X = 1, Y = 0, iota(X, Y, Z), format('iota(~w, ~w) is ~w~n', [X, Y, Z]).
+:- X = 1, Y = 4, iota(X, Y, Z), format('iota(~w, ~w) is ~w~n', [X, Y, Z]).
+:- X = 1, Y = 1, iota(X, Y, Z), format('iota(~w, ~w) is ~w~n', [X, Y, Z]).
+:- X = 4, Y = 10, iota(X, Y, Z), format('iota(~w, ~w) is ~w~n', [X, Y, Z]).
