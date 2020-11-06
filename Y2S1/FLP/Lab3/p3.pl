485fa42f7eaff59b6f260263e4294c69b65d5b77
Alex Ionescu <alxi.2001@gmail.com>
2020-11-06 09:04:23 +0200

    Add examples & extra requirement

--- Y2S1/FLP/Lab3/p3.pl
+++ Y2S1/FLP/Lab3/p3.pl
@@ -11,6 +11,11 @@ unique([], []).
 unique([A, A | As], L) :- unique([A | As], L).
 unique([A | As], [A | L]) :- unique(As, L).
 
+:- L = [], unique(L, Lu), format('unique(~w) is ~w~n', [L, Lu]).
+:- L = [1], unique(L, Lu), format('unique(~w) is ~w~n', [L, Lu]).
+:- L = [1, 1], unique(L, Lu), format('unique(~w) is ~w~n', [L, Lu]).
+:- L = [1, 1, 2, 3, 4, 4, 5], unique(L, Lu), format('unique(~w) is ~w~n~n', [L, Lu]).
+
 % merge(a1..am, b1..bn) =
 %   { b1..bn, if m = 0
 %   ; a1..am, if n = 0
@@ -24,10 +29,20 @@ merge(L, [], L).
 merge([A | As], [B | Bs], [A | L]) :- A < B, merge(As, [B | Bs], L).
 merge([A | As], [B | Bs], [B | L]) :- merge([A | As], Bs, L).
 
+:- A = [1, 2], B = [], merge(A, B, L), format('merge(~w, ~w) is ~w~n', [A, B, L]).
+:- A = [], B = [1, 2], merge(A, B, L), format('merge(~w, ~w) is ~w~n', [A, B, L]).
+:- A = [1, 2, 2, 3], B = [1, 4, 4], merge(A, B, L), format('merge(~w, ~w) is ~w~n', [A, B, L]).
+:- A = [1, 2, 3, 7, 10], B = [1, 2, 4, 4, 4, 8, 15], merge(A, B, L), format('merge(~w, ~w) is ~w~n~n', [A, B, L]).
+
 % mergeUnique(a1..am, b1..bn) = unique(merge(a1..am, b1..bn))
 
 % (i, i, o)
 mergeUnique(As, Bs, Lu) :- merge(As, Bs, L), unique(L, Lu).
+
+:- A = [1, 2], B = [], mergeUnique(A, B, L), format('mergeUnique(~w, ~w) is ~w~n', [A, B, L]).
+:- A = [], B = [1, 2], mergeUnique(A, B, L), format('mergeUnique(~w, ~w) is ~w~n', [A, B, L]).
+:- A = [1, 2, 2, 3], B = [1, 4, 4], mergeUnique(A, B, L), format('mergeUnique(~w, ~w) is ~w~n', [A, B, L]).
+:- A = [1, 2, 3, 7, 10], B = [1, 2, 4, 4, 4, 8, 15], mergeUnique(A, B, L), format('mergeUnique(~w, ~w) is ~w~n~n', [A, B, L]).
 
 % b)
 
@@ -41,3 +56,28 @@ mergeUnique(As, Bs, Lu) :- merge(As, Bs, L), unique(L, Lu).
 mergeLists([], []).
 mergeLists([A | As], M) :- number(A), mergeLists(As, M).
 mergeLists([As | Es], L) :- mergeLists(Es, M), mergeUnique(As, M, L).
+
+:- L = [], mergeLists(L, Lm), format('mergeLists(~w) is ~w~n', [L, Lm]).
+:- L = [1, 2, 3, 4, 5, 5, 5, 7], mergeLists(L, Lm), format('mergeLists(~w) is ~w~n', [L, Lm]).
+:- L = [1, 2, [], [], [1, 2, 2], 4, [0, 3, 4], 4], mergeLists(L, Lm), format('mergeLists(~w) is ~w~n', [L, Lm]).
+:-
+  L = [1, [2, 3], 4, 5, [1, 4, 6], 3, [1, 3, 7, 9, 10], 5, [1, 1, 11], 8],
+  mergeLists(L, Lm),
+  format('mergeLists(~w) is ~w~n~n', [L, Lm]).
+
+% Extra requirement
+% Compute the product of all elements from a list
+
+% product(a1..an) =
+%   { 1, if n = 0
+%   ; a1 * product(a2..an), otherwise
+%   }
+
+% (i, o)
+product([], 1).
+product([A | As], P) :- product(As, Ps), P is A * Ps.
+
+:- L = [], product(L, P), format('product(~w) is ~w~n', [L, P]).
+:- L = [2, 2], product(L, P), format('product(~w) is ~w~n', [L, P]).
+:- L = [1, 2, 4, 9], product(L, P), format('product(~w) is ~w~n', [L, P]).
+:- L = [1, 2, 3, 0], product(L, P), format('product(~w) is ~w~n~n', [L, P]).
