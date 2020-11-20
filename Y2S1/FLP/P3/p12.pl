bab32b4f76b2b2c0ed0d0701a490982152bad954
Alex Ionescu <alxi.2001@gmail.com>
2020-11-20 03:11:09 +0200

    Add FLP P3

---
+++ Y2S1/FLP/P3/p12.pl
@@ -0,0 +1,20 @@
+% valid(x) = x ∈ { -1, 0, 1 }
+% Flow model: (i)
+valid(X) :- member(X, [-1, 0, 1]).
+
+% validDiff(x) = x ∈ { 1, 2 }
+% Flow model: (i)
+validDiff(X) :- member(X, [1, 2]).
+
+% string(n) =
+%   1. [], if n = 0
+%   2. [x], if n = 1 and valid(x)
+%   3. a ⋃ b ⋃ l, if n >= 2 and valid(a) and valid(b) and validDiff(b - a) and string(n - 1, b ⋃ l)
+% Flow model: (i, o)
+string(0, []).
+string(1, [H]) :- valid(H).
+string(N, [H1, H2 | T]) :- N >= 2, valid(H1), valid(H2), D is H2 - H1, validDiff(D), M is N - 1, string(M, [H2 | T]).
+
+% strings(n) = ⋃ string(n, l)
+% Flow model: (i, o)
+strings(N, L) :- findall(X, string(N, X), L).
