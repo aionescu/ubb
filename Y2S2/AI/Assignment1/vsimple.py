6602d8994ec32f55184f66f6257b2f2e99a898b7
Alex Ionescu <alxi.2001@gmail.com>
2021-03-02 02:27:15 +0200

    Make some progress on AI Lab1

--- Y2S2/AI/Assignment1/vsimple.py
+++ Y2S2/AI/Assignment1/vsimple.py
@@ -1,11 +1,9 @@
-
-
 # import the pygame module, so you can use it
 import pickle,pygame,sys
 from pygame.locals import *
 from random import random, randint
 import numpy as np
-
+from time import sleep
 
 #Creating some colors
 BLUE  = (0, 0, 255)
@@ -21,7 +19,7 @@ DOWN = 2
 LEFT = 1
 RIGHT = 3
 
-#define indexes variations 
+#define indexes variations
 v = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
 
@@ -30,13 +28,13 @@ class Environment():
         self.__n = 20
         self.__m = 20
         self.__surface = np.zeros((self.__n, self.__m))
-    
+
     def randomMap(self, fill = 0.2):
         for i in range(self.__n):
             for j in range(self.__m):
                 if random() <= fill :
                     self.__surface[i][j] = 1
-                
+
     def __str__(self):
         string=""
         for i in range(self.__n):
@@ -44,10 +42,10 @@ class Environment():
                 string = string + str(int(self.__surface[i][j]))
             string = string + "\n"
         return string
-                
+
     def readUDMSensors(self, x,y):
         readings=[0,0,0,0]
-        # UP 
+        # UP
         xf = x - 1
         while ((xf >= 0) and (self.__surface[xf][y] == 0)):
             xf = xf - 1
@@ -67,14 +65,14 @@ class Environment():
         while ((yf >= 0) and (self.__surface[x][yf] == 0)):
             yf = yf - 1
             readings[RIGHT] = readings[RIGHT] + 1
-     
+
         return readings
-    
+
     def saveEnvironment(self, numFile):
         with open(numFile,'wb') as f:
             pickle.dump(self, f)
             f.close()
-        
+
     def loadEnvironment(self, numfile):
         with open(numfile, "rb") as f:
             dummy = pickle.load(f)
@@ -82,7 +80,7 @@ class Environment():
             self.__m = dummy.__m
             self.__surface = dummy.__surface
             f.close()
-        
+
     def image(self, colour = BLUE, background = WHITE):
         imagine = pygame.Surface((420,420))
         brick = pygame.Surface((20,20))
@@ -92,10 +90,10 @@ class Environment():
             for j in range(self.__m):
                 if (self.__surface[i][j] == 1):
                     imagine.blit(brick, ( j * 20, i * 20))
-                
-        return imagine        
-        
-        
+
+        return imagine
+
+
 class DMap():
     def __init__(self):
         self.__n = 20
@@ -104,8 +102,8 @@ class DMap():
         for i in range(self.__n):
             for j in range(self.__m):
                 self.surface[i][j] = -1
-        
-        
+
+
     def markDetectedWalls(self, e, x, y):
         #   To DO
         # mark on this map the wals that you detect
@@ -117,7 +115,7 @@ class DMap():
                 i = i - 1
         if (i>=0):
             self.surface[i][y] = 1
-            
+
         i = x + 1
         if wals[DOWN] > 0:
             while ((i < self.__n) and (i <= x + wals[DOWN])):
@@ -125,7 +123,7 @@ class DMap():
                 i = i + 1
         if (i < self.__n):
             self.surface[i][y] = 1
-            
+
         j = y + 1
         if wals[LEFT] > 0:
             while ((j < self.__m) and (j <= y + wals[LEFT])):
@@ -133,7 +131,7 @@ class DMap():
                 j = j + 1
         if (j < self.__m):
             self.surface[x][j] = 1
-        
+
         j = y - 1
         if wals[RIGHT] > 0:
             while ((j >= 0) and (j >= y - wals[RIGHT])):
@@ -141,113 +139,148 @@ class DMap():
                 j = j - 1
         if (j >= 0):
             self.surface[x][j] = 1
-        
+
         return None
-        
+
     def image(self, x, y):
-        
+
         imagine = pygame.Surface((420,420))
         brick = pygame.Surface((20,20))
         empty = pygame.Surface((20,20))
         empty.fill(WHITE)
         brick.fill(BLACK)
         imagine.fill(GRAYBLUE)
-        
+
         for i in range(self.__n):
             for j in range(self.__m):
                 if (self.surface[i][j] == 1):
                     imagine.blit(brick, ( j * 20, i * 20))
                 elif (self.surface[i][j] == 0):
                     imagine.blit(empty, ( j * 20, i * 20))
-                
+
         drona = pygame.image.load("drona.png")
         imagine.blit(drona, (y *20, x*20))
         return imagine
-        
-        
+
+
 class Drone():
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def move(self, detectedMap):
-        pressed_keys = pygame.key.get_pressed()
-        if self.x > 0:
-            if pressed_keys[K_UP] and detectedMap.surface[self.x-1][self.y]==0:
-                self.x = self.x - 1
-        if self.x < 19:
-            if pressed_keys[K_DOWN] and detectedMap.surface[self.x+1][self.y]==0:
-                self.x = self.x + 1
-        
-        if self.y > 0:
-              if pressed_keys[K_LEFT]and detectedMap.surface[self.x][self.y-1]==0:
-                  self.y = self.y - 1
-        if self.y < 19:        
-              if pressed_keys[K_RIGHT] and detectedMap.surface[self.x][self.y+1]==0:
-                  self.y = self.y + 1
-                  
-    def moveDSF(self, detectedMap):
-        pass
-         # TO DO!
-         #rewrite this function in such a way that you perform an automatic 
-         # mapping with DFS           
-                  
+        self.pos = (x, y)
+        self.seen = []
+        self.stack = []
+
+    # def move(self, detectedMap):
+    #     pressed_keys = pygame.key.get_pressed()
+    #     if self.x > 0:
+    #         if pressed_keys[K_UP] and detectedMap.surface[self.x-1][self.y]==0:
+    #             self.x = self.x - 1
+    #     if self.x < 19:
+    #         if pressed_keys[K_DOWN] and detectedMap.surface[self.x+1][self.y]==0:
+    #             self.x = self.x + 1
+
+    #     if self.y > 0:
+    #           if pressed_keys[K_LEFT]and detectedMap.surface[self.x][self.y-1]==0:
+    #               self.y = self.y - 1
+    #     if self.y < 19:
+    #           if pressed_keys[K_RIGHT] and detectedMap.surface[self.x][self.y+1]==0:
+    #               self.y = self.y + 1
+
+    def x(self):
+      return self.pos[0]
+
+    def y(self):
+      return self.pos[1]
+
+    def nearby(self, m):
+      x, y = self.pos
+      (xMax, yMax) = m.shape
+
+      l = []
+      ps = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
+
+      for (x, y) in ps:
+        if x >= 0 and x < xMax and y >= 0 and y < yMax and m[x][y] == 0 and (x, y) not in self.seen and (x, y) not in self.stack:
+          l.append((x, y))
+
+      return l
+
+    def backtrack(self):
+      if not self.crumbs:
+        return
+
+      self.pos = self.crumbs.pop()
+
+    def moveDFS(self, map):
+      self.seen.append(self.pos)
+
+      nearby = self.nearby(map.surface)
+      self.stack.extend(nearby)
+
+      if self.stack:
+        self.pos = self.stack.pop()
+
 # define a main function
 def main():
     #we create the environment
     e = Environment()
     e.loadEnvironment("test2.map")
     #print(str(e))
-    
+
     # we create the map
-    m = DMap() 
-    
-    
+    m = DMap()
+
     # initialize the pygame module
     pygame.init()
-    # load and set the logo
+
     logo = pygame.image.load("logo32x32.png")
     pygame.display.set_icon(logo)
     pygame.display.set_caption("drone exploration")
-        
-    
-    
+
+
+
     # we position the drone somewhere in the area
     x = randint(0, 19)
     y = randint(0, 19)
-    
+
     #cream drona
     d = Drone(x, y)
-    
-    
-    
+
+
+
     # create a surface on screen that has the size of 800 x 480
     screen = pygame.display.set_mode((800,400))
     screen.fill(WHITE)
     screen.blit(e.image(), (0,0))
-    
+
     # define a variable to control the main loop
     running = True
-     
+
     # main loop
+    timeLastFrame = 0
+    timeSinceDFS = 0
+
     while running:
-        # event handling, gets all event from the event queue
-        for event in pygame.event.get():
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
-            if event.type == KEYDOWN:
-                # use this function instead of move
-                #d.moveDSF(m)
-                d.move(m)
-        m.markDetectedWalls(e, d.x, d.y)
-        screen.blit(m.image(d.x,d.y),(400,0))
-        pygame.display.flip()
-       
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+      currentTime = pygame.time.get_ticks()
+      deltaTime = currentTime - timeLastFrame
+      timeLastFrame = currentTime
+      timeSinceDFS += deltaTime
+
+      if timeSinceDFS < 100:
+        continue
+
+      timeSinceDFS = 0
+      d.moveDFS(m)
+      m.markDetectedWalls(e, d.x(), d.y())
+      screen.blit(m.image(d.x(),d.y()),(400,0))
+      pygame.display.flip()
+
     pygame.quit()
-     
-     
+
+
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
