fe4e9f91abb25c3aef179181c96d84a04d2701fd
Alex Ionescu <alxi.2001@gmail.com>
2021-03-17 12:40:31 +0200

    Add DBMSs Lab1WinForm

---
+++ Y2S2/DBMS/Lab1/Lab1WinForm/Lab1WinForm/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Lab1WinForm {
+
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.label1 = new System.Windows.Forms.Label();
+      this.pkgGridView = new System.Windows.Forms.DataGridView();
+      this.label2 = new System.Windows.Forms.Label();
+      this.pkgVerGridView = new System.Windows.Forms.DataGridView();
+      this.updateBtn = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.pkgGridView)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pkgVerGridView)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Fira Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(13, 13);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(89, 20);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Packages";
+      // 
+      // pkgGridView
+      // 
+      this.pkgGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.pkgGridView.Location = new System.Drawing.Point(17, 37);
+      this.pkgGridView.Name = "pkgGridView";
+      this.pkgGridView.Size = new System.Drawing.Size(824, 150);
+      this.pkgGridView.TabIndex = 1;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Fira Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(13, 254);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(159, 20);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "PackageVersions";
+      // 
+      // pkgVerGridView
+      // 
+      this.pkgVerGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.pkgVerGridView.Location = new System.Drawing.Point(17, 278);
+      this.pkgVerGridView.Name = "pkgVerGridView";
+      this.pkgVerGridView.Size = new System.Drawing.Size(824, 150);
+      this.pkgVerGridView.TabIndex = 3;
+      // 
+      // updateBtn
+      // 
+      this.updateBtn.Font = new System.Drawing.Font("Fira Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.updateBtn.Location = new System.Drawing.Point(17, 488);
+      this.updateBtn.Name = "updateBtn";
+      this.updateBtn.Size = new System.Drawing.Size(133, 31);
+      this.updateBtn.TabIndex = 4;
+      this.updateBtn.Text = "Update DB";
+      this.updateBtn.UseVisualStyleBackColor = true;
+      this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(853, 580);
+      this.Controls.Add(this.updateBtn);
+      this.Controls.Add(this.pkgVerGridView);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.pkgGridView);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pkgGridView)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pkgVerGridView)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.DataGridView pkgGridView;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.DataGridView pkgVerGridView;
+    private System.Windows.Forms.Button updateBtn;
+  }
+}
+
---
+++ Y2S2/DBMS/Lab1/Lab1WinForm/Lab1WinForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿namespace Lab1WinForm {
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel;
+  using System.Data;
+  using System.Drawing;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+  using System.Windows.Forms;
+  using System.Data.SqlClient;
+
+  public partial class Form1: Form {
+    SqlConnection _conn;
+    DataSet _dataSet;
+    SqlDataAdapter _pkgAdapter, _pkgVerAdapter;
+    SqlCommandBuilder _cmdBuilder;
+    BindingSource _pkgBindingSource, _pkgVerBindingSourcew;
+
+    public Form1() => InitializeComponent();
+
+    void Form1_Load(object sender, EventArgs e) {
+      _conn = new SqlConnection(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = PackageManager; Integrated Security = SSPI;");
+      _dataSet = new DataSet();
+
+      _pkgAdapter = new SqlDataAdapter("select * from Packages", _conn);
+      _pkgVerAdapter = new SqlDataAdapter("select * from PackageVersions", _conn);
+
+      _cmdBuilder = new SqlCommandBuilder(_pkgVerAdapter);
+
+      _pkgAdapter.Fill(_dataSet, "Packages");
+      _pkgVerAdapter.Fill(_dataSet, "PackageVersions");
+
+      var dr = new DataRelation(
+        "fk_PackageVersions_Packages",
+        _dataSet.Tables["Packages"].Columns["id"],
+        _dataSet.Tables["PackageVersions"].Columns["package"]);
+
+      _dataSet.Relations.Add(dr);
+
+      _pkgBindingSource = new BindingSource {
+        DataSource = _dataSet,
+        DataMember = "Packages"
+      };
+
+      _pkgVerBindingSourcew = new BindingSource {
+        DataSource = _pkgBindingSource,
+        DataMember = "fk_PackageVersions_Packages"
+      };
+
+      pkgGridView.DataSource = _pkgBindingSource;
+      pkgVerGridView.DataSource = _pkgVerBindingSourcew;
+    }
+
+    void updateBtn_Click(object sender, EventArgs e) => _pkgVerAdapter.Update(_dataSet, "PackageVersions");
+  }
+}
