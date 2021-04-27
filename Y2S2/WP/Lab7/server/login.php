23ef330e00aba5882d1607efb05c94e02f164804
Alex Ionescu <alxi.2001@gmail.com>
2021-04-28 01:08:45 +0300

    Add WP Lab7

---
+++ Y2S2/WP/Lab7/server/login.php
@@ -0,0 +1,17 @@
+<?php
+  $conn = mysqli_connect("localhost", "root", "", "uri_collection");
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $query = "SELECT * FROM Users WHERE password = '$password'";
+  $result = mysqli_query($conn, $query);
+
+  if (mysqli_num_rows($result) == 0)
+    echo "false";
+  else {
+    $row = mysqli_fetch_array($result);
+    setcookie("userID", $row["id"], time() + 86400 * 7, "/");
+    echo "true";
+  }
+?>
