2c122f2ef1b5a9b0891b085f555c6187655424e5
Alex Ionescu <alxi.2001@gmail.com>
2021-10-12 11:37:56 +0300

    Update PDP Lab2

--- Y3S1/PDP/Lab2/Main.hs
+++ Y3S1/PDP/Lab2/Main.hs
@@ -2,13 +2,17 @@
 
 module Main where
 
-import Control.Concurrent(forkFinally)
+import Control.Concurrent(forkFinally, threadDelay)
 import Control.Concurrent.MVar(MVar, newEmptyMVar, putMVar, takeMVar)
-import Data.Foldable(traverse_)
+import Data.Foldable(traverse_, for_)
 import Data.List(transpose)
+import System.IO (hFlush, stdout, hSetBuffering, BufferMode (LineBuffering))
 
 readVecs :: String -> [[Integer]]
 readVecs = transpose . ((read <$>) . words <$>) . lines
+
+print' :: String -> IO ()
+print' s = putStrLn s *> hFlush stdout
 
 fork :: IO () -> IO (MVar ())
 fork th = do
@@ -18,17 +22,25 @@ fork th = do
 
 producer :: MVar (Maybe Integer) -> [[Integer]] -> IO ()
 producer m vs = do
-  traverse_ (putMVar m . Just . product) vs
+  for_ vs \v -> do
+    threadDelay 500000
+    print' $ "Producer sending: " ++ show v
+    putMVar m $ Just $ product v
+
   putMVar m Nothing
 
 consumer :: MVar (Maybe Integer) -> Integer -> IO ()
 consumer m acc =
   takeMVar m >>= \case
-    Nothing -> print acc
-    Just v -> consumer m $ acc + v
+    Nothing -> print' $ "Final sum: " ++ show acc
+    Just v -> do
+      print' $ "Consumer received: " ++ show v
+      consumer m $ acc + v
 
 main :: IO ()
 main = do
+  hSetBuffering stdout LineBuffering
+
   vs <- readVecs <$> readFile "Vecs.txt"
   m <- newEmptyMVar
 
