8847d059abb0ad66aa65ea9b0e00bf9c23bfa19d
Alex Ionescu <alxi.2001@gmail.com>
2021-10-17 02:25:57 +0300

    Add PKC Lab1

---
+++ Y3S1/PKC/Lab1/GCD.hs
@@ -0,0 +1,42 @@
+#!/usr/bin/env runhaskell
+{-# OPTIONS_GHC -Wall -Wextra #-}
+{-# LANGUAGE TypeApplications #-}
+
+module GCD(main) where
+
+import Data.List(sort, transpose)
+import Prelude hiding (gcd)
+
+-- powerOf n f = (m, p) such that p ^ f * m = n
+powerOf :: Integer -> Integer -> (Integer, Integer)
+powerOf n f
+  | n `rem` f == 0 = (+ 1) <$> powerOf (n `quot` f) f
+  | otherwise = (n, 0)
+
+-- factors n = [(f1, p1), ..., (fi, pi)] such that f1 ^ p1 * ... * fi ^ pi = n
+factors :: Integer -> [(Integer, Integer)]
+factors x = go [2 .. x `quot` 2] x
+  where
+    go [] _ = []
+    go (f : fs) n
+      | p > 0 = (f, p) : go fs n'
+      | otherwise = go fs n
+      where (n', p) = powerOf n f
+
+-- Assuming sorted input
+gcd' :: [Integer] -> Integer
+gcd' [] = 1
+gcd' (n : ns) = r
+  where
+    (fs, ps) = unzip $ factors n
+    fs' = ps : ((\x -> snd . powerOf x <$> fs) <$> ns)
+    r = product $ zipWith (^) fs $ minimum <$> transpose fs'
+
+gcd :: [Integer] -> Integer
+gcd = gcd' . sort
+
+main :: IO ()
+main = do
+  print $ gcd [10, 20, 30]
+  print $ gcd [10, 15, 30]
+  print $ gcd [22, 10, 30]
