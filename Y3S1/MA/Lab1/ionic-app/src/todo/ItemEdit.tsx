e4b89180cca0eaaf91a35718b15514907d5d9bd0
Alex Ionescu <alxi.2001@gmail.com>
2021-10-25 17:32:43 +0300

    Add MA Lab1 Ionic app

---
+++ Y3S1/MA/Lab1/ionic-app/src/todo/ItemEdit.tsx
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonLoading,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import { getLogger } from '../core';
+import { ItemContext } from './ItemProvider';
+import { RouteComponentProps } from 'react-router';
+import { ItemProps } from './ItemProps';
+
+const log = getLogger('ItemEdit');
+
+interface ItemEditProps extends RouteComponentProps<{
+  id?: string;
+}> {}
+
+const ItemEdit: React.FC<ItemEditProps> = ({ history, match }) => {
+  const { items, saving, savingError, saveItem } = useContext(ItemContext);
+  const [text, setText] = useState('');
+  const [item, setItem] = useState<ItemProps>();
+  useEffect(() => {
+    log('useEffect');
+    const routeId = match.params.id || '';
+    const item = items?.find(it => it.id === routeId);
+    setItem(item);
+    if (item) {
+      setText(item.text);
+    }
+  }, [match.params.id, items]);
+  const handleSave = () => {
+    const editedItem = item ? { ...item, text } : { text };
+    saveItem && saveItem(editedItem).then(() => history.goBack());
+  };
+  log('render');
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Edit</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={handleSave}>
+              Save
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonInput value={text} onIonChange={e => setText(e.detail.value || '')} />
+        <IonLoading isOpen={saving} />
+        {savingError && (
+          <div>{savingError.message || 'Failed to save item'}</div>
+        )}
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default ItemEdit;
---
+++ Y3S1/MA/Lab1/ionic-app/src/todo/ItemList.tsx
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import { RouteComponentProps } from 'react-router';
+import {
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonIcon,
+  IonList, IonLoading,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import { add } from 'ionicons/icons';
+import Item from './Item';
+import { getLogger } from '../core';
+import { ItemContext } from './ItemProvider';
+
+const log = getLogger('ItemList');
+
+const ItemList: React.FC<RouteComponentProps> = ({ history }) => {
+  const { items, fetching, fetchingError } = useContext(ItemContext);
+  log('render');
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>My App</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonLoading isOpen={fetching} message="Fetching items" />
+        {items && (
+          <IonList>
+            {items.map(({ id, text}) =>
+              <Item key={id} id={id} text={text} onEdit={id => history.push(`/item/${id}`)} />)}
+          </IonList>
+        )}
+        {fetchingError && (
+          <div>{fetchingError.message || 'Failed to fetch items'}</div>
+        )}
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={() => history.push('/item')}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default ItemList;
