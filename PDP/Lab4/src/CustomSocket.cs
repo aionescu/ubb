0b771d86bb9972d86686fa91f3b23e8775662251
Alex Ionescu <alxi.2001@gmail.com>
2021-11-15 20:00:24 +0200

    Add PDP Lab4 base

---
+++ PDP/Lab4/src/CustomSocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab4.domain {
+  class CustomSocket {
+    // event atomic variables needed for task implementations
+    public ManualResetEvent connectDone = new ManualResetEvent(false);
+    public ManualResetEvent sendDone = new ManualResetEvent(false);
+    public ManualResetEvent receiveDone = new ManualResetEvent(false);
+
+    public Socket sock = null; // socket to connect to server
+
+    public const int BUFF_SIZE = 1024; // 1024 bytes
+
+    public byte[] buffer = new byte[BUFF_SIZE];
+
+    public StringBuilder responseContent = new StringBuilder();
+
+    public int id; // keep a unique id for each custom socket
+    public string hostname;
+    public string endpoint;
+
+    public IPEndPoint remoteEndPoint; // ip of website endpoint (address+port)
+  }
+}
---
+++ PDP/Lab4/src/Parser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.utils {
+  class Parser {
+    public static int PORT = 80; // http default port
+
+    public static string GetRequestString(string hostname, string endpoint) {
+      return "GET " + endpoint + " HTTP/1.1\r\n" +
+             "Host: " + hostname + "\r\n" +
+             "Content-Length: 0\r\n\r\n";
+    }
+
+    public static int GetContentLen(string respContent) {
+      var contentLen = 0;
+      var respLines = respContent.Split('\r', '\n');
+      foreach (string respLine in respLines) {
+        var headDetails = respLine.Split(':');
+
+        if (String.Compare(headDetails[0], "Content-Length", StringComparison.Ordinal) == 0) {
+          contentLen = int.Parse(headDetails[1]);
+        }
+      }
+
+      return contentLen;
+    }
+
+    public static bool ResponseHeaderObtained(string responseContent) {
+      return responseContent.Contains("\r\n\r\n");
+    }
+  }
+}
