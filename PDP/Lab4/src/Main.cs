5670ff413039f29339f2f19187e9bd9f8fc069a5
Alex Ionescu <alxi.2001@gmail.com>
2021-11-23 11:21:26 +0200

    Store downloaded files in CallbackImpl

--- PDP/Lab4/src/Main.cs
+++ PDP/Lab4/src/Main.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 List<string> hosts = new() {
   "cs.ubbcluj.ro/~rlupsa/edu/pdp",
-  "math.ubbcluj.ro/~crivei/",
   "haskell.org",
   "cataas.com/cat/12",
 };
@@ -11,8 +10,8 @@ List<string> hosts = new() {
 Console.WriteLine("Callbacks");
 CallbackImpl.Run(hosts);
 
-Console.WriteLine("\nTPL");
-TaskImpl.Run(hosts);
+// Console.WriteLine("\nTPL");
+// TaskImpl.Run(hosts);
 
-Console.WriteLine("\nTPL Async");
-TaskAsyncImpl.Run(hosts);
+// Console.WriteLine("\nTPL Async");
+// TaskAsyncImpl.Run(hosts);
