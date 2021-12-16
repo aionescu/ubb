02b9311a3669257ca84e264173b86281ec617a92
Alex Ionescu <alxi.2001@gmail.com>
2021-12-16 16:19:58 +0200

    Compute factors of `n` from factors of `k * n`

--- PKC/Lab3/GFermat.hs
+++ PKC/Lab3/GFermat.hs
@@ -1,10 +1,14 @@
-{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wall -Wno-type-defaults #-}
 {-# LANGUAGE ViewPatterns, BlockArguments, TypeApplications #-}
 
 import Control.Monad(when)
 import Data.Foldable(for_)
-import System.Environment(getArgs)
 import System.Exit(exitSuccess)
+import Text.Printf(printf)
+
+(//) :: Integer -> Integer -> Integer
+(//) = quot
+infixl 7 //
 
 sqrt' :: Integer -> Integer
 sqrt' = floor . sqrt @Double . fromIntegral
@@ -16,24 +20,33 @@ isPerfSquare n = n == s * s
 
 main :: IO ()
 main = do
-  [read -> b, read -> n] <- getArgs
+  let n = 2 ^ 51 - 1
+  let b = 100
 
   for_ [1 ..] \k -> do
     let t₀ = sqrt' (k * n)
 
-    putStrLn $ "k = " <> show k
-    putStrLn $ "t₀ = " <> show t₀
+    printf "k = %i\n" k
+    printf "t₀ = %i\n" t₀
 
     for_ [1 .. b] \i -> do
       let t = t₀ + i
       let s² = t * t - k * n
 
-      putStrLn $ "t = t₀ + " <> show i <> " ⟹  t² - " <> show k <> " * n = " <> show s²
+      printf "t = t₀ + %i ⟹ t² - %i * n = %i\n" i k s²
 
       when (isPerfSquare s²) do
         let s = sqrt' s²
 
-        putStrLn $ "\nn = 1/" <> show k <> " * " <> show (t - s) <> " * " <> show (t + s)
+        let (fk₁, fk₂) = (t - s, t + s)
+        printf "\nn = 1/%i * %i * %i\n" k fk₁ fk₂
+
+        let e = gcd k fk₁
+        let e2 = k // e
+
+        let (f₁, f₂) = (fk₁ // e, fk₂ // e2)
+        printf "n = %i * %i\n" f₁ f₂
+
         exitSuccess
 
     putStrLn ""
