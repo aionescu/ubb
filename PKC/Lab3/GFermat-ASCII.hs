613d2e865711a26049dffc2832085a0d33390d52
Alex Ionescu <alxi.2001@gmail.com>
2021-12-30 16:48:51 +0200

    Add ASCII version

--- PKC/Lab3/GFermat-ASCII.hs
+++ PKC/Lab3/GFermat-ASCII.hs
@@ -24,28 +24,28 @@ main = do
   let b = 100
 
   for_ [1 ..] \k -> do
-    let t₀ = sqrt' (k * n)
+    let t0 = sqrt' (k * n)
 
     printf "k = %i\n" k
-    printf "t₀ = %i\n" t₀
+    printf "t0 = %i\n" t0
 
     for_ [1 .. b] \i -> do
-      let t = t₀ + i
-      let s² = t * t - k * n
+      let t = t0 + i
+      let s2 = t * t - k * n
 
-      printf "t = t₀ + %i ⟹ t² - %i * n = %i\n" i k s²
+      printf "t = t0 + %i => t ^ 2 - %i * n = %i\n" i k s2
 
-      when (isPerfSquare s²) do
-        let s = sqrt' s²
+      when (isPerfSquare s2) do
+        let s = sqrt' s2
 
-        let (fk₁, fk₂) = (t - s, t + s)
-        printf "\nn = 1/%i * %i * %i\n" k fk₁ fk₂
+        let (fk1, fk2) = (t - s, t + s)
+        printf "\nn = 1/%i * %i * %i\n" k fk1 fk2
 
-        let e = gcd k fk₁
+        let e = gcd k fk1
         let e2 = k // e
 
-        let (f₁, f₂) = (fk₁ // e, fk₂ // e2)
-        printf "n = %i * %i\n" f₁ f₂
+        let (f1, f2) = (fk1 // e, fk2 // e2)
+        printf "n = %i * %i\n" f1 f2
 
         exitSuccess
 
