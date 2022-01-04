721f91508a0c1c4ebb50212506d84c817d2692b8
Alex Ionescu <alxi.2001@gmail.com>
2022-01-04 03:35:49 +0200

    Add MA Android Node server

---
+++ MA/Android/node-server/src/index.js
@@ -0,0 +1,43 @@
+import Koa from 'koa';
+import WebSocket from 'ws';
+import http from 'http';
+import Router from 'koa-router';
+import bodyParser from "koa-bodyparser";
+import { timingLogger, exceptionHandler, jwtConfig, initWss, verifyClient } from './utils';
+import { router as noteRouter } from './note';
+import { router as authRouter } from './auth';
+import jwt from 'koa-jwt';
+import cors from '@koa/cors';
+
+const app = new Koa();
+const server = http.createServer(app.callback());
+const wss = new WebSocket.Server({ server });
+initWss(wss);
+
+app.use(cors());
+app.use(timingLogger);
+app.use(exceptionHandler);
+app.use(bodyParser());
+
+const prefix = '/api';
+
+// public
+const publicApiRouter = new Router({ prefix });
+publicApiRouter
+  .use('/auth', authRouter.routes());
+app
+  .use(publicApiRouter.routes())
+  .use(publicApiRouter.allowedMethods());
+
+app.use(jwt(jwtConfig));
+
+// protected
+const protectedApiRouter = new Router({ prefix });
+protectedApiRouter
+  .use('/item', noteRouter.routes());
+app
+  .use(protectedApiRouter.routes())
+  .use(protectedApiRouter.allowedMethods());
+
+server.listen(3000);
+console.log('started on port 3000');
